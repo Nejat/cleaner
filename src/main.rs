61712e65cb7d984a14e2db78c58492c992b1bba1
nejat <nejatoz@gmail.com>
2022-04-14 08:41:56 -0400

    [release] updated version for new release
    *  switched platform matching to case insensitive
    * added platform validation

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@
 #![allow(clippy::module_name_repetitions)]
 #![allow(clippy::items_after_statements)]
 // ==============================================================
-#![doc(html_root_url = "https://docs.rs/cleaner/0.6.0")]
+#![doc(html_root_url = "https://docs.rs/cleaner/0.7.0")]
 
 #[macro_use]
 extern crate clap;
